5241fd87ef19c94ceddf51dfd3345e65bd2a0938
Shubhra Potdar <shubrapotdar2518@gmail.com>
2026-01-16 22:06:52 +0530

    Add files via upload
    Add factorial of number by using recursion program

---
+++ factorial_recursion.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int factorial(int n) {
+    if (n == 0 || n == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return n * factorial(n - 1);
+    } 
+}
+int main() {
+    int number;
+    printf("Enter a number:");
+    scanf("%d", &number);
+
+    if (number < 0)
+    {
+        printf("Factorial is not defined for negative number.\n");
+    }
+    else
+    {
+        printf("Factorial of %d is %d\n", number, factorial(number));
+    }
+    return 0;    
+}
